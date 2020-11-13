39b030f7e8c6a728bff904787d6e9d6ab6157c7b
Blaire <grant.blaire@gmail.com>
2020-11-12 18:45:52 -0600

    commit tree 17ecb210f4bcc0ad9b8a483f3d60098441a89236

--- grandma.rb
+++ grandma.rb
@@ -1,11 +1,17 @@
 
-phrase = gets.chomp
-
-if phrase == phrase.upcase
-    puts "NO, NOT SINCE 1938!"
-elsif phrase == "I LOVE YOU GRANDMA!"
-    puts "I LOVE YOU TOO PUMPKIN!"
-else 
-    puts "HUH?! SPEAK UP, SONNY!"  
+def speak_to_grandma(phrase)
+    if phrase == "I LOVE YOU GRANDMA!"
+       "I LOVE YOU TOO PUMPKIN!"
+    elsif phrase.upcase == phrase
+       "NO, NOT SINCE 1938!"
+    else
+       "HUH?! SPEAK UP, SONNY!"  
+    end
 end  
+
+
+
+
+
+
 
